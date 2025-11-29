0aa9b7972da9e4135a1e3c9de1634093884e4ddd
javier marin <nano.world.contact@gmail.com>
2025-11-28 22:21:32 -0600

    chore: removing logs [release]

--- src/utils/pathFactory.ts
+++ src/utils/pathFactory.ts
@@ -1,7 +1,5 @@
 export const pathFactory = (baseURL: string, path: string) => {
   const url = new URL(baseURL);
-  console.log('base pathname', url.pathname);
   url.pathname = url.pathname === '/' ? path : `${url.pathname}${path}`;
-  console.log('new pathname', url.pathname);
   return url;
 };
